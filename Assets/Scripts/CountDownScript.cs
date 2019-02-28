e7c71e7547d91145c1ed65c24b6f7e89a7c42527
OceanDiaali <ocean.diaali@gmail.com>
2019-02-28 19:38:07 +0100

    first commit

---
+++ Assets/Scripts/CountDownScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownScript : MonoBehaviour {
+
+	public float timeLeft = 15.0f;
+	public Text startText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		startText.text = (timeLeft).ToString ("0");
+		if (timeLeft <= 5) {
+			startText.color = Color.red;
+		}
+		if (timeLeft < 0) {
+			timeLeft = 0;
+			startText.text = "Time Up!";
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private AudioSource source;
+	public Rigidbody2D rgbd2d;
+
+	// Use this for initialization
+	void Start () {
+		rgbd2d = GetComponent<Rigidbody2D> ();
+		source = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter2D (Collision2D coll) {
+		string gmObj = coll.gameObject.name;
+		switch (gmObj) {
+		case "coin":
+			source.Play ();
+			Destroy (coll.gameObject);
+			ScoreScript.scoreValue++;
+			break;
+
+		case "coin2":
+			source.Play ();
+			Destroy (coll.gameObject);
+			ScoreScript.scoreValue++;
+			break;
+
+		case "coin3":
+			source.Play ();
+			Destroy (coll.gameObject);
+			ScoreScript.scoreValue++;
+			break;
+
+		case "coin4":
+			source.Play ();
+			Destroy (coll.gameObject);
+			ScoreScript.scoreValue++;
+			break;
+
+		case "coin5":
+			source.Play ();
+			Destroy (coll.gameObject);
+			ScoreScript.scoreValue++;
+			break;
+
+		}
+		/*if (coll.gameObject.name == "coin") 
+		{
+			Destroy (coll.gameObject);
+			ScoreScript.scoreValue++;
+		}*/
+	}
+	
+	// update per frame
+	void Update() {
+		transform.Rotate (0, 0, speed);
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Vector2 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+		} else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Vector2 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		} else if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector2 position = this.transform.position;
+			position.y++;
+			this.transform.position = position;
+		} else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Vector2 position = this.transform.position;
+			position.y--;
+			this.transform.position = position;
+		}
+	}
+}
